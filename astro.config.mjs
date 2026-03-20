fc664e2db2d3129954fb9d0125000e0afb6fc973
Daniel-Cochachi <daniel.cochachi.pariona@gmail.com>
2026-03-20 16:30:50 -0500

    Fix build: Update Node version and Astro syntax

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import icon from 'astro-icon';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://Daniel-Cochachi.github.io',
-  base: 'Portafolio-Daniel-Cochachi',
+  base: '/Portafolio-Daniel-Cochachi',
   integrations: [icon()],
 });
